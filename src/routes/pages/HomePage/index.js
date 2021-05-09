ee0007bdec1337200dd18dfc20a5eca0e1bc7b03
AaroonNineleaps <aaroon.goel@nineleaps.com>
2021-05-09 19:14:33 +0530

    demo

--- src/routes/pages/HomePage/index.js
+++ src/routes/pages/HomePage/index.js
@@ -15,7 +15,9 @@ export const HomePage = () => {
 
       <div className="display-tiles">
         <DisplayTile image={foodSVG} heading="Food" description={sampText} />
+        <DisplayTile image={foodSVG} heading="Food" description={sampText} />
       </div>
+      
     </>
   );
 };
